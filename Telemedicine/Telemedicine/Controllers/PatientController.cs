7028e0b9decab37409bd3e27a1b5f5355fb75181
bhuppi1993 <33427340+bhuppi1993@users.noreply.github.com>
2020-05-08 16:47:53 +0530

    submited by bhupendra

---
+++ Telemedicine/Telemedicine/Controllers/PatientController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Telemedicine.Controllers
+{
+    public class PatientController : Controller
+    {
+        // GET: Patient
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult PatientFamilyList()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+    }
+}
---
+++ Telemedicine/Telemedicine/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Telemedicine.MvcApplication" Language="C#" %>
